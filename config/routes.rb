4159340a2ec72682ce063a0f0f296dea4b354d60
project <>
2022-12-01 10:34:45 +0800

    change routes for client and admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "members#index"
-  constraints(ClientDomainConstraint.new) do
-    resources :members do
-    end
-  end
+  #
+
   constraints(AdminDomainConstraint.new) do
-    namespace :admin do
+    namespace :admin, path: '' do
+      devise_for :users, controller: {
+        sessions: 'admins/sessions'
+      }
+      root to: "home#index"
       resources :users
     end
+  end
+  constraints(ClientDomainConstraint.new) do
+    devise_for :users, controller: {
+      sessions: 'users/sessions', registration: 'users/registrations'
+    }
+    root to: "members#index"
+    resource :members do
+      get "update_profile"
+    end
+    # resources :members
   end
 end
