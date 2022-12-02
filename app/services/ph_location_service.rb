aefe977bcdf0b92fcb8348e9bd1bd2cb8d92cb5b
project <>
2022-12-02 15:40:36 +0800

    fetch district and province

--- app/services/ph_location_service.rb
+++ app/services/ph_location_service.rb
@@ -14,4 +14,60 @@ class PhLocationService
       address_region.save
     end
   end
+
+
+  def fetch_provinces
+    request = RestClient.get("#{url}/provinces")
+    data = JSON.parse(request.body)
+    data.each do |province|
+      address_province = Address::Province.find_or_initialize_by(code: province['code'])
+      region = Address::Region.find_by_code(province['regionCode'])
+      address_province.region = region
+      address_province.name = province['name']
+      address_province.save
+    end
+  end
+
+  def fetch_district
+    request = RestClient.get("#{url}/districts")
+    data = JSON.parse(request.body)
+    data.each do |district|
+      address_district = Address::District.find_or_initialize_by(code: district['code'])
+      region = Address::Region.find_by_code(district['regionCode'])
+      address_district.region = region
+      address_district.name = district['name']
+      address_district.save
+    end
+  end
+
+  def fetch_city_municipality
+    request = RestClient.get("#{url}/cities-municipalities")
+    data = JSON.parse(request.body)
+    data.each do |city_municipality|
+      address_city_municipality = Address::CityMunicipality.find_or_initialize_by(code: city_municipality['code'])
+      if city_municipality['provinceCode']
+        province = Address::Province.find_by_code(city_municipality['provinceCode'])
+        address_city_municipality.province = province
+        address_city_municipality.name = city_municipality['name']
+        address_city_municipality.save
+      elsif city_municipality['districtCode']
+        district = Address::District.find_by_code(city_municipality['districtCode'])
+        address_city_municipality.district = district
+        address_city_municipality.name = city_municipality['name']
+        address_city_municipality.save
+      else
+        if city_municipality['name'] == "City of Isabela"
+          province = Address::Province.find_by_name('Basilan')
+          Address::CityMunicipality.find_or_create_by(code: city_municipality['code'], name: city_municipality['name'], province: province)
+          address_city_municipality.province = province
+          address_city_municipality.save
+        elsif city_municipality['name'] == "City of Cotabato"
+          province = Address::Province.find_by_name('Maguindanao')
+          Address::CityMunicipality.find_or_create_by(code: city_municipality['code'], name: city_municipality['name'], province: province)
+          address_city_municipality.province = province
+          address_city_municipality.save
+        end
+      end
+    end
+  end
 end
