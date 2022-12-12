9f4a3cdfd2077b34a75f9796d7fbc77e6d024c9e
project <>
2022-12-12 17:36:43 +0800

    check schema if database item is indicated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_07_051728) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_050344) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "code"
@@ -83,6 +83,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_07_051728) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "items", charset: "utf8mb4", force: :cascade do |t|
+    t.string "image"
+    t.string "name"
+    t.integer "quantity"
+    t.integer "minimum_bets"
+    t.integer "batch_count"
+    t.integer "status"
+    t.string "state"
+    t.datetime "online_at"
+    t.datetime "offline_at"
+    t.datetime "start_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "members", charset: "utf8mb4", force: :cascade do |t|
